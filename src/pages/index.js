9a61703fd1fe19ae8e990e78ee461ac767e7b79b
Meehoi <beam_baimon@hotmail.com>
2020-05-15 22:12:30 +0700

    Config

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,18 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import styles from "../styles/pages/index.module.scss"
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div class={styles.index}>
+      <h1>
+        CS
+        <br />
+        CMU
+      </h1>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
